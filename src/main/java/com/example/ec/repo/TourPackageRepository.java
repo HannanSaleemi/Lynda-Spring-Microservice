5d34879573c59549b051d389fa291b481493c59d
Hannan Saleemi <hannan@octopuslabs.com>
2019-12-21 07:44:48 +0000

    Don't expose Save and Delete Methods for each Tour / TourPackage + Change /tourPackages to /packages

--- src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -3,12 +3,38 @@ package com.example.ec.repo;
 import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
     // Adding a query method to find a tour package by name
     Optional<TourPackage> findByName(@Param("name")String name);
 
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
